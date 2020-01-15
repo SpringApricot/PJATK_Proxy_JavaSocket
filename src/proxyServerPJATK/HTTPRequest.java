70c3c02db40c715cc0b1be09c66940831986335e
Joanna <azia54@gmail.com>
2020-01-16 00:01:54 +0100

    Revert "Revert "Division between image and text""
    This reverts commit ec83224f8dbe1d524fdb9fb37f307584d7513389.

--- src/proxyServerPJATK/HTTPRequest.java
+++ src/proxyServerPJATK/HTTPRequest.java
@@ -1,7 +1,6 @@
 package proxyServerPJATK;
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -50,6 +49,7 @@ public class HTTPRequest {
 		}
 		
 		headers.put("Accept-Encoding", "identity");
+		headers.put("Connection", "close");
 	}
 	
 	public HTTPResponse forward() throws UnknownHostException, IOException {
